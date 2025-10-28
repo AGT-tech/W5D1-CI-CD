b777e0ea7916b21e39baae65f290e31de1e53a07
AGT-tech <abil.teshome@maine.edu>
2025-10-27 20:13:03 -0400

    Add tests folder

---
+++ tests/test_calculator.py
@@ -0,0 +1,12 @@
+from app.calculator import add, divide
+import pytest
+import math 
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_divide():
+    assert divide(10, 2) == 5
+    with pytest.raises(ValueError):
+        divide(10, 0)
